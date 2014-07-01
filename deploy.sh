9f3c358e8528a2db4aeaf8a0c373bbb7405629bc
codec <codec@fnord.cx>
2014-07-01 17:40:04 +0200

    refactor deploy.sh to play nicely w/ hookshot

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-cd /var/lib/hubot
-git pull
-npm install
-service hubot stop
-service hubot start
+deploy() {
+  cd /var/lib/hubot
+  git pull
+
+  npm install
+
+  # FIXME
+  # calling stop/start because restart will
+  # result in multiple hubot instances ...
+  service hubot stop
+  service hubot start
+}
+
+(deploy &) >/dev/null 2>&1
+
+trap "{ printf 'HTTP/1.1 200\r\n\r\nThanks, bro.\n'; exit 0; }" EXIT
